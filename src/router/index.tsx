a7804a3cc2bc97055cc8c6d7d3eafba530941ef1
SangJin983 <lazistar983@gmail.com>
2026-01-05 00:14:27 +0900

    feat: MainLayout 을 라우터에 적용

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,24 @@
 import { createBrowserRouter } from "react-router";
 
+import { MainLayout } from "@/layouts/MainLayout";
 import { HomePage, LoginPage, SettingsPage } from "@/pages";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/settings",
-    element: <SettingsPage />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/settings",
+        element: <SettingsPage />,
+      },
+    ],
   },
 ]);
